1953413438d4eba88c5d4a9d0b3220c21090119a
Shamvil <shamvil@qburst.com>
2020-05-13 13:35:57 +0530

    Initial Commit

---
+++ SonarDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SonarDemoApp
+//
+//  Created by Shamvil on 13/05/20.
+//  Copyright © 2020 Shamvil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
